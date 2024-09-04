b8ace183c54e5003c04e40ce29022f0aca4d574b
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-09-04 19:31:42 +0900

    registry: organization repository

---
+++ registry/src/main/java/com/example/registry/repo/OrganizationRepository.java
@@ -0,0 +1,6 @@
+package com.example.registry.repo;
+
+import com.example.registry.domain.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {}
