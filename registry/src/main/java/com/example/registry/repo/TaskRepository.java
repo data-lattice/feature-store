6f2791f60d4d27191b6244a2b52e881f29d99e03
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-10-10 09:39:01 +0900

    registry: task repository

---
+++ registry/src/main/java/com/example/registry/repo/TaskRepository.java
@@ -0,0 +1,6 @@
+package com.example.registry.repo;
+
+import com.example.registry.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {}
