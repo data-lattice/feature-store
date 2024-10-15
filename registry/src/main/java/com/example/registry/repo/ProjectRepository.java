6bac7960236d72324f1e6b1228572b853e4ed5eb
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-10-15 09:16:16 +0900

    more wip

--- registry/src/main/java/com/example/registry/repo/ProjectRepository.java
+++ registry/src/main/java/com/example/registry/repo/ProjectRepository.java
@@ -4,3 +4,4 @@ import com.example.registry.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {}
+// minor wording
