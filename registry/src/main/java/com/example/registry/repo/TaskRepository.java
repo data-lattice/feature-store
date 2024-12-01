15bac397f0f0132534b80592c1ab0e718d779998
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-12-01 19:17:31 +0900

    add docstring

--- registry/src/main/java/com/example/registry/repo/TaskRepository.java
+++ registry/src/main/java/com/example/registry/repo/TaskRepository.java
@@ -4,3 +4,4 @@ import com.example.registry.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {}
+// check perf here
