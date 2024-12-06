6b6b58b3e2024406094197f3b075b1a5af401c3d
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-12-06 11:43:02 +0900

    registry: invoice repository

---
+++ registry/src/main/java/com/example/registry/repo/InvoiceRepository.java
@@ -0,0 +1,6 @@
+package com.example.registry.repo;
+
+import com.example.registry.domain.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {}
