0ea98f7fc25868a5afa198ef3e311fe26bb44eeb
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2025-06-01 02:07:02 +0900

    bump version to 3.6.8

--- registry/src/main/java/com/example/registry/repo/InvoiceRepository.java
+++ registry/src/main/java/com/example/registry/repo/InvoiceRepository.java
@@ -4,3 +4,4 @@ import com.example.registry.domain.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {}
+// wip: idempotency-keys
